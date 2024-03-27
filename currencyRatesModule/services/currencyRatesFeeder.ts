a9b82f706e9bd372c755b9b10cf54e0f495c9ae4
wWrongName <wwr0ngn4m3@gmail.com>
2024-03-27 13:12:01 +0300

    feat: add db interaction for feeder

--- currencyRatesModule/services/currencyRatesFeeder.ts
+++ currencyRatesModule/services/currencyRatesFeeder.ts
@@ -1,55 +1,106 @@
+import { TICKERS_MODEL } from '../../common/models/tickers'
 import { CurrencyRateSchema } from '../schemas/currencySchemas'
+import { CurrencyRate, Token } from '../../common/models/currencies'
+import { AppDataSource } from '../../common/configs/typeORMConfig'
 
 const CURRENCYFREAKS_URL = process.env.CURRENCYFREAKS_URL
 const CURRENCYFREAKS_TOKEN = process.env.CURRENCYFREAKS_TOKEN
 
+async function getLastRate(): Promise<Date> {
+    const currencyRateRepository = AppDataSource.getRepository(CurrencyRate)
+    const lastRecord = await currencyRateRepository.findOne({
+        order: {
+            date: 'DESC',
+        },
+    })
+    if (lastRecord) return lastRecord.date
+    return new Date(0)
+}
+
+async function createToken(ticker: string) {
+    const tokenRepository = AppDataSource.getRepository(Token)
+    const newToken = new Token()
+    newToken.ticker = ticker
+    return tokenRepository.save(newToken)
+}
+
+async function getTokenByTicker(ticker: string) {
+    const tokenRepository = AppDataSource.getRepository(Token)
+    const token = await tokenRepository.findOne({ where: { ticker: ticker } })
+    if (!token) return await createToken(ticker)
+    return token
+}
+
+async function getTokensAll(rates: Record<string, any>) {
+    let tokens: Record<string, any> = {}
+    for (let ticker of Object.keys(rates)) {
+        tokens[ticker] = await getTokenByTicker(ticker)
+    }
+    return tokens
+}
+
+async function writeCurrencyRates(rates: Record<string, any>) {
+    const tokens = await getTokensAll(rates)
+    const date = new Date()
+
+    await AppDataSource.transaction('READ COMMITTED', async transactionalEntityManager => {
+        for (let ticker in rates) {
+            const currencyRate = new CurrencyRate()
+            currencyRate.date = date
+            currencyRate.token = tokens[ticker]
+            currencyRate.usdRate = rates[ticker]
+            transactionalEntityManager.save(currencyRate)
+        }
+    })
+}
+
 export class CurrencyRatesFeeder {
     readonly UPD_TIMEOUT: number = 60 * 1000
+    readonly SYMBOLS: string = TICKERS_MODEL.join(',')
 
-    async run() {
-        if (await this.isUpdTime()) await this.updateCurrencyRates()
+    public async run(): Promise<void> {
+        const lastUpdTime = await this.getLastUpdTime()
+        if (this.isUpdTime(lastUpdTime)) await this.updateCurrencyRates()
         setTimeout(this.run.bind(this), this.UPD_TIMEOUT)
     }
 
-    async isUpdTime() {
+    private isUpdTime(lastUpdTime: Date): boolean {
         const currentTime = new Date()
         const hours = currentTime.getHours()
         const minutes = currentTime.getMinutes()
         console.trace(`New UPD Tick. Time: ${currentTime}. ${hours}:${minutes}`)
 
-        const lastUpdTime = await this.getLastUpdTime()
-        const diffTime = currentTime.getTime() - lastTime.getTime()
-        const diffHours = diffTime / (60 * 60 * 1000)
+        const diffTime = currentTime.getTime() - lastUpdTime.getTime()
+        const hour = 60 * 60 * 1000
+        const diffHours = diffTime / hour
+        console.trace(`Last update: ${diffHours}h ago`)
 
-        if (diffHours > 12)
-            return true
+        if (diffHours > 12) return true
         return hours === 12 && minutes === 0
     }
 
-    async getLastUpdTime(): Promise<Date> {
-        // TODO - check
+    private async getLastUpdTime(): Promise<Date> {
+        return await getLastRate()
     }
 
-    async updateCurrencyRates(): Promise<void> {
+    private async updateCurrencyRates(): Promise<void> {
         try {
-            const rates = this.fetchCurrencyRates()
-            // TODO - handle rates
-            // TODO - write into DB
+            const rates = await this.fetchCurrencyRates()
+            await writeCurrencyRates(rates)
         } catch (error) {
             console.error(error)
         }
     }
 
-    async fetchCurrencyRates(): Promise<object> {
-        const targetUrl = `${CURRENCYFREAKS_URL}/latest&apikey=${CURRENCYFREAKS_TOKEN}`
+    private async fetchCurrencyRates(): Promise<object> {
+        const targetUrl = `${CURRENCYFREAKS_URL}/latest&apikey=${CURRENCYFREAKS_TOKEN}&symbols=${this.SYMBOLS}`
         console.trace('Sending request to fetch currency rates:', targetUrl)
 
         const response = await fetch(targetUrl)
-        const currencyRates = <object>await response.json()
+        const currencyData = <any>await response.json()
+        console.trace('Extracting the currency rates response:', currencyData)
 
-        console.trace('Extracting the currency rates response:', currencyRates)
-        await CurrencyRateSchema.validateAsync(currencyRates)
-
-        return currencyRates
+        await CurrencyRateSchema.validateAsync(currencyData.rates)
+        return currencyData.rates
     }
 }
